428554647a86218320c92f17e7a67ad1160629d7
Lane Aasen <laneaasen@gmail.com>
2013-05-07 21:34:20 -0700

    adds to_coords which uses the google maps api to get coords from an address

---
+++ to_coords.py
@@ -0,0 +1,25 @@
+
+import requests
+
+maps_api_endpoint = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false'
+
+class FetchError(Exception):
+    def __init__(self, resp):
+        self.resp = resp
+
+    def __str__(self):
+        return '%s returned invalid status code %s' % (self.resp.url, self.resp.status_code)
+
+def address_to_coords(loc):
+    resp = requests.get(maps_api_endpoint % loc)
+    
+    if resp.status_code == requests.codes.ok:
+        results = resp.json()['results']
+
+        if len(results) > 0:
+            return results[0]['geometry']['location']
+    else:
+        raise FetchError(resp)
+
+print address_to_coords('seattle')
+print address_to_coords('asdasdljsadfoiuru')
