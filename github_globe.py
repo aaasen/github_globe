6d661e8a2e35f52777bbf617d1c76333ed6a7917
Lane Aasen <laneaasen@gmail.com>
2013-05-09 18:52:59 -0700

    somewhat functional prototype of bulk loc to coord translation

--- github_globe.py
+++ github_globe.py
@@ -1,10 +1,12 @@
 
 import requests
 import requests_cache
-
-maps_api_endpoint = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false'
+import json
 
 requests_cache.install_cache('cache/coords_cache')
+
+maps_api_endpoint = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false'
+loc_data_endpoint = 'http://commondatastorage.googleapis.com/github_globe/location_frequencies'
 
 class FetchError(Exception):
     def __init__(self, resp):
@@ -12,6 +14,24 @@ class FetchError(Exception):
 
     def __str__(self):
         return '%s returned invalid status code %s' % (self.resp.url, self.resp.status_code)
+
+def safe_json_loads(string):
+    try:
+        return json.loads(string.encode('utf-8'))
+    except:
+        return None
+
+def fetch_loc_data():
+    resp = requests.get(loc_data_endpoint)
+    print 'request done'
+
+    if resp.status_code == requests.codes.ok:
+        locs = resp.text.split()
+        json_locs = map(lambda x: safe_json_loads(x), locs)
+        return filter(lambda x: x is not None, json_locs)
+
+    else:
+        raise FetchError(resp)
 
 def address_to_coords(loc):
     resp = requests.get(maps_api_endpoint % loc)
@@ -24,5 +44,20 @@ def address_to_coords(loc):
     else:
         raise FetchError(resp)
 
-print address_to_coords('seattle')
-print address_to_coords('asdasdljsadfoiuru')
+    print 'address done'
+
+def bulk_address_to_coords(data):
+    data = data[:10]
+    data = filter(lambda x: type(x) is dict, data)
+
+    for x in data:
+        x['coords'] = address_to_coords(x['actor_attributes_location'])
+        del x['actor_attributes_location']
+
+    return data
+
+
+data = bulk_address_to_coords(fetch_loc_data())
+
+with open('user_locations.json', 'w') as f:
+    f.write(json.dumps(data))
