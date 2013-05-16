cab770c9a72287984c78f2a5afba193516bc2fb1
Lane Aasen <laneaasen@gmail.com>
2013-05-16 09:11:33 -0700

    adds geocoord class, which is a container for lat and long

---
+++ transform/geo_coord.py
@@ -0,0 +1,14 @@
+
+class GeoCoord:
+    def __init__(self, lat, lng):
+        self.lat = lat
+        self.lng = lng
+
+    def __eq__(self, other):
+        return self.lat == other.lat and self.lng == other.lng
+
+    def __hash__(self):
+        return hash((self.lat, self.lng))
+
+    def round(self, ndigits=0):
+        return GeoCoord(round(self.lat, ndigits), round(self.lng, ndigits))
