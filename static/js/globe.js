da5688153ba087276906090e3d8627e30387a1ff
Lane Aasen <laneaasen@gmail.com>
2013-05-16 14:10:29 -0700

    changes directory structure

--- static/js/globe.js
+++ static/js/globe.js
@@ -41,7 +41,7 @@ DAT.Globe = function(container, colorFn) {
         'varying vec2 vUv;',
         'void main() {',
           'vec3 diffuse = texture2D( texture, vUv ).xyz;',
-          'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
+          'float intensity = 1.0 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
           'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
           'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
         '}'
@@ -71,7 +71,7 @@ DAT.Globe = function(container, colorFn) {
 
   var overRenderer;
 
-  var imgDir = '/globe/';
+  var imgDir = '/static/img/';
 
   var curZoomSpeed = 0;
   var zoomSpeed = 50;
